5ff93ba7d0109fbfc8e5759b74a28ae3ba7e0017
Cruz, Justin <cruzj9@wit.edu>
2018-09-22 19:19:19 -0400

    added date/time formating

--- src/main/java/com/jcruz/Event_Planner_Dropwizard/Event_PlannerApplication.java
+++ src/main/java/com/jcruz/Event_Planner_Dropwizard/Event_PlannerApplication.java
@@ -1,5 +1,10 @@
 package com.jcruz.Event_Planner_Dropwizard;
 
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+
+import com.jcruz.Event_Planner_Dropwizard.resources.EventResources;
+
 import io.dropwizard.Application;
 import io.dropwizard.setup.Bootstrap;
 import io.dropwizard.setup.Environment;
@@ -8,6 +13,7 @@ public class Event_PlannerApplication extends Application<Event_PlannerConfigura
 
     public static void main(final String[] args) throws Exception {
         new Event_PlannerApplication().run(args);
+        
     }
 
     @Override
@@ -23,7 +29,13 @@ public class Event_PlannerApplication extends Application<Event_PlannerConfigura
     @Override
     public void run(final Event_PlannerConfiguration configuration,
                     final Environment environment) {
-        // TODO: implement application
+        // Date/Time formating 
+    	DateFormat eventDateFormat = new SimpleDateFormat(configuration.getDateFormat());
+        environment.getObjectMapper().setDateFormat(eventDateFormat);
+        
+        EventResources eventResource = new EventResources();
+        environment.jersey().register(eventResource);
+    	
     }
 
 }
--- src/main/java/com/jcruz/Event_Planner_Dropwizard/Event_PlannerConfiguration.java
+++ src/main/java/com/jcruz/Event_Planner_Dropwizard/Event_PlannerConfiguration.java
@@ -7,4 +7,16 @@ import javax.validation.constraints.*;
 
 public class Event_PlannerConfiguration extends Configuration {
     // TODO: implement service configuration
+	
+	
+	@NotEmpty
+	private String dateFormat;
+	
+	public String getDateFormat() {
+		return dateFormat;
+	}
 }
+
+
+
+
